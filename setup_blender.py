1e30e38a5f987474aa147963688556a6a9c9e9bf
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2024-04-03 17:37:09 +0100

    add blender setup script

---
+++ setup_blender.py
@@ -0,0 +1,17 @@
+
+import os
+import sys
+import bpy
+import pip
+
+# install pyyaml
+pip.main(['install', 'pyyaml', '--user'])
+
+# install addon
+addon_path = os.path.abspath(sys.argv[-1])
+if addon_path.endswith(".zip"):
+    bpy.ops.preferences.addon_install(filepath=addon_path, overwrite=True)
+    bpy.ops.wm.addon_enable(module='SequenceDataLoader')
+    bpy.ops.wm.save_userpref()
+else:
+    print("Please provide the path to the addon .zip as argument")
