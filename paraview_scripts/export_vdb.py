ca2fcefabca0631f8759c762c833fe82cb6a6e35
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2024-04-15 17:29:26 +0100

    add arguments to vdb export

--- paraview_scripts/export_vdb.py
+++ paraview_scripts/export_vdb.py
@@ -30,38 +30,50 @@ def update_file(filename, export_path, samplingBounds, samplingDimensions, cellS
         This can be null but then cellSizes will need to be populated
     :param cellSize: Cell size for all dimensions for paraview
     """ 
+    print("reading ", filename)
     # create a new 'XDMF Reader'
-    xmf_reader = XDMFReader(FileNames=[filename]) 
+    if filename.endswith("xdmf"):
+        reader = XDMFReader(FileNames=[filename]) 
+    elif filename.endswith("vtu"):
+        reader = XMLUnstructuredGridReader(FileName=[filename])
+    else:
+        file_list = os.listdir(filename)
+        file_list.sort()
+        file_list = [os.path.join(filename, x) for x in file_list]
+        reader = XMLUnstructuredGridReader(FileName=file_list)
+
     
     # create index list of timesteps to be used
     animationScene1 = GetAnimationScene()
-    timestep_list = animationScene1.TimeKeeper.TimestepValues
+    timeKeeper = GetTimeKeeper()
+    #jtimestep_list = animationScene1.TimeKeeper.TimestepValues
+    timestep_list = timeKeeper.TimestepValues
 
     # create a new 'Resample To Image' with sampling bounds and dimensions 
-    resampleToImage1 = ResampleToImage(registrationName='ResampleToImage1', Input=xmf_reader)
+    resampleToImage1 = ResampleToImage(registrationName='ResampleToImage1', Input=reader)
 
     # chose file extension for output.
-    format="vdb"
+    format = "vdb"
     if kwargs and ("format" in kwargs):
-            format=kwargs["format"]
+            format = kwargs["format"]
 
-    if samplingBounds != None:
+    if samplingBounds:
         resampleToImage1.UseInputBounds = 0
         resampleToImage1.SamplingBounds = samplingBounds 
     else: 
         resampleToImage1.UseInputBounds = 1 # use paraview's default sampling bounds 
 
     # obtain samplingDimensions from cell sizes or directly from arguments
-    if samplingDimensions == None:
+    if not samplingDimensions:
         samplingDimensions = [] 
-        NDIM = 3 # number of dimensions by default 
-        if cellSize != None :
-            for i in range(NDIM):
-                samplingDimensions.append(int(abs(resampleToImage1.SamplingBounds[2*i+1] - resampleToImage1.SamplingBounds[2*i])/cellSize))
+        if cellSize:
+            samplingBounds = resampleToImage1.SamplingBounds
+            samplingDimensions = [ int((samplingBounds[2*i+1] - samplingBounds[2*i])/cellSize) for i in range(3) ]
         else:
             raise Exception("Need either cell sizes or sampling dimensions. Both can't be empty.")
-    else:
-        resampleToImage1.SamplingDimensions = samplingDimensions 
+
+    print("Sampling dimensions: ", samplingDimensions)
+    resampleToImage1.SamplingDimensions = samplingDimensions 
 
     # update timeline for the first timestep onwards 
     UpdatePipeline(time=timestep_list[0], proxy=resampleToImage1) 
@@ -70,18 +82,23 @@ def update_file(filename, export_path, samplingBounds, samplingDimensions, cellS
     for step, time in enumerate(timestep_list):
 
         output_filename = get_filename(export_path, step,format)
-        if os.path.isfile(output_filename):
+        output_filename_tmp = output_filename + "tmp"
+        if os.path.isfile(output_filename) or os.path.isfile(output_filename_tmp):
             continue 
 
-        if os.path.isdir(output_filename):
-            raise Exception(f"Error, {output_filename} can't be a directory.")
+        # Create empty temporary file
+        with open(output_filename_tmp, 'w') as fp:
+            pass
 
         print("  - Export timestep {}/{} ({})".format(step, len(timestep_list), time))
 
         # set time for that animation
         animationScene1.AnimationTime = time 
 
+        UpdatePipeline(time=time, proxy=resampleToImage1)
+
         save_data(output_filename, resampleToImage1)
+        os.remove(output_filename_tmp)
 
 def save_data(output_name, resampleToImage1):
     """
@@ -91,25 +108,36 @@ def save_data(output_name, resampleToImage1):
     :param resampleToImage1: paraview filter object
     """
     array_vals = resampleToImage1.PointData.keys()
-    cell_vals= resampleToImage1.CellData.keys() 
+    cell_vals = resampleToImage1.CellData.keys() 
     SaveData(output_name, proxy=resampleToImage1,
              LookupTable=None, 
              PointDataArrays=array_vals,
              CellDataArrays=cell_vals
-    ) 
+             ) 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--datapath", required=True, help="Path to the solution data")
-    parser.add_argument("--exportpath", default="paraview_export", help="Path to export vdb files")
+    parser.add_argument("--data-path", required=True, help="Path to the solution data")
+    parser.add_argument("--export-path", default="paraview_export", help="Path to export vdb files")
+    parser.add_argument("--sampling-bounds", help="Bounds of the box to export, comma separated list of values xmin,xmax,ymin...")
+    parser.add_argument("--sampling-dims", help="Number of cells in x, y and z")
+    parser.add_argument("--cell-size", help="Size of each cell (to use instead of --sampling-dims)")
     args = parser.parse_args()
 
-    samplingBounds = None
     samplingDimensions = None
-    cellSize = 0.5
+    samplingBounds = None
+    cellSize = None
+    if args.sampling_bounds:
+        samplingBounds = [ float(x) for x in args.sampling_bounds.split(',') ]
+
+    if args.sampling_dims:
+        samplingDimensions = [ int(x) for x in args.sampling_dims.split(',') ]
+
+    if args.cell_size:
+        cellSize = float(args.cell_size)
 
     # create export directory if it doesn't exist
-    Path(args.exportpath).mkdir(parents=True, exist_ok=True) 
+    Path(args.export_path).mkdir(parents=True, exist_ok=True) 
 
-    update_file(args.datapath, args.exportpath, samplingBounds, samplingDimensions, cellSize)
+    update_file(args.data_path, args.export_path, samplingBounds, samplingDimensions, cellSize)
